859545380ead35460cddb45eb22f26732779f98b
tatsianakarpesh <tatsiana.karpesh@nwboxed.com>
2025-11-03 13:45:48 +0000

    Added postgress

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,13 +1,45 @@
 class CarsController < ApplicationController
-  before_action :set_car, only: %i[ show edit update destroy ]
+  before_action :set_car, only: %i[ show ]
+  before_action :deny_access, only: %i[ new create edit update destroy ]
 
   # GET /cars or /cars.json
   def index
-    @cars = Car.all
+    @cars = Car.all.includes(:cart_items)  # Add includes to optimize queries
+
+    # Handle search
     if params[:search].present?
       search_term = params[:search].upcase
       @cars = @cars.where("UPPER(make) LIKE ? OR UPPER(model) LIKE ?", "%#{search_term}%", "%#{search_term}%")
     end
+
+    # Filter by color
+    @cars = @cars.where("UPPER(description) LIKE ?", "%#{params[:color].upcase}%") if params[:color].present?
+
+    # Filter by year
+    @cars = @cars.where(year: params[:year]) if params[:year].present?
+
+    # Handle sorting (with default sort)
+    @current_sort = params[:sort] || 'year_desc'
+    @cars = case @current_sort
+            when 'year_asc'
+              @cars.order('year ASC, make ASC')
+            when 'year_desc'
+              @cars.order('year DESC, make ASC')
+            when 'price_asc'
+              @cars.order('price ASC, make ASC')
+            when 'price_desc'
+              @cars.order('price DESC, make ASC')
+            else
+              @cars.order('year DESC, make ASC')
+            end
+
+    # Get unique years for filter dropdown
+    @available_years = Car.distinct.pluck(:year).sort.reverse
+
+    # Get unique colors from descriptions
+    @available_colors = %w[Red Blue Black White Silver Gray Green]
+
+    # Apply pagination after sorting
     @cars = @cars.page(params[:page]).per(20)
   end
 
@@ -17,59 +49,36 @@ class CarsController < ApplicationController
 
   # GET /cars/new
   def new
-    @car = Car.new
+    deny_access
   end
 
   # GET /cars/1/edit
   def edit
+    deny_access
   end
 
-  # POST /cars or /cars.json
+  # POST /cars
   def create
-    @car = Car.new(car_params)
-
-    respond_to do |format|
-      if @car.save
-        format.html { redirect_to @car, notice: "Car was successfully created." }
-        format.json { render :show, status: :created, location: @car }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
-      end
-    end
+    deny_access
   end
 
-  # PATCH/PUT /cars/1 or /cars/1.json
+  # PATCH/PUT /cars/1
   def update
-    respond_to do |format|
-      if @car.update(car_params)
-        format.html { redirect_to @car, notice: "Car was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @car }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
-      end
-    end
+    deny_access
   end
 
-  # DELETE /cars/1 or /cars/1.json
+  # DELETE /cars/1
   def destroy
-    @car.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to cars_path, notice: "Car was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    deny_access
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_car
-      @car = Car.find(params.expect(:id))
+      @car = Car.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def car_params
-      params.expect(car: [ :make, :model, :year, :price, :description, :image_url ])
+    def deny_access
+      redirect_to cars_path, alert: "Access denied. Users are not allowed to modify cars."
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_11_03_104038) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
