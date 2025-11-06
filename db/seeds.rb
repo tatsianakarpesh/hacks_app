2ade43ad77f0694215e6a9ad78da1738ca3cf1b5
tatsianakarpesh <tatsiana.karpesh@nwboxed.com>
2025-11-06 14:50:40 +0100

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,85 +11,85 @@
 puts "Cleaning up existing cars..."
 Car.destroy_all
 
-TARGET_COUNT = 10_000
-BATCH_SIZE = 500
+TARGET_COUNT = 1_000
 
-car_data = [
-  ['Toyota', 'Camry', 'sedan'],
-  ['Honda', 'Civic', 'sedan'],
-  ['Ford', 'Mustang', 'sports'],
-  ['Tesla', 'Model 3', 'electric'],
-  ['BMW', 'X5', 'suv'],
-  ['Chevrolet', 'Silverado', 'pickup'],
-  ['Audi', 'A4', 'sedan'],
-  ['Mercedes-Benz', 'C-Class', 'luxury'],
-  ['Volkswagen', 'Golf', 'hatchback'],
-  ['Hyundai', 'Tucson', 'suv'],
-  ['Kia', 'Sportage', 'suv'],
-  ['Mazda', 'CX-5', 'suv'],
-  ['Nissan', 'Altima', 'sedan'],
-  ['Subaru', 'Outback', 'wagon'],
-  ['Jeep', 'Wrangler', 'suv'],
-  ['Lexus', 'RX 350', 'suv']
+# Extended list of car manufacturers and their models
+CARS_DATA = {
+  'Audi' => ['A3', 'A4', 'A6', 'Q3', 'Q5', 'Q7', 'RS6', 'e-tron'],
+  'BMW' => ['3 Series', '5 Series', 'X3', 'X5', 'M3', 'M5', 'i4', 'iX'],
+  'Mercedes' => ['C-Class', 'E-Class', 'S-Class', 'GLC', 'GLE', 'AMG GT', 'EQS'],
+  'Volkswagen' => ['Golf', 'Passat', 'Tiguan', 'Atlas', 'ID.4', 'Arteon'],
+  'Porsche' => ['911', 'Cayenne', 'Macan', 'Panamera', 'Taycan'],
+  'Tesla' => ['Model 3', 'Model S', 'Model X', 'Model Y', 'Cybertruck'],
+  'Toyota' => ['Camry', 'Corolla', 'RAV4', 'Highlander', 'Supra', 'Prius'],
+  'Honda' => ['Civic', 'Accord', 'CR-V', 'Pilot', 'HR-V'],
+  'Ford' => ['Mustang', 'F-150', 'Explorer', 'Bronco', 'Mach-E'],
+  'Chevrolet' => ['Camaro', 'Silverado', 'Tahoe', 'Corvette', 'Bolt'],
+  'Hyundai' => ['Tucson', 'Santa Fe', 'Elantra', 'Ioniq 5', 'Palisade'],
+  'Kia' => ['Sportage', 'Telluride', 'EV6', 'K5', 'Sorento'],
+  'Volvo' => ['XC40', 'XC60', 'XC90', 'S60', 'C40'],
+  'Mazda' => ['CX-5', 'CX-9', 'Mazda3', 'Mazda6', 'MX-5'],
+  'Lexus' => ['RX', 'NX', 'ES', 'IS', 'LC'],
+  'Jaguar' => ['F-Pace', 'I-Pace', 'XF', 'F-Type'],
+  'Land Rover' => ['Range Rover', 'Defender', 'Discovery', 'Evoque'],
+  'Subaru' => ['Outback', 'Forester', 'WRX', 'Crosstrek'],
+  'Acura' => ['MDX', 'RDX', 'TLX', 'Integra'],
+  'Infiniti' => ['Q50', 'QX50', 'QX60', 'QX80']
+}
+
+COLORS = [
+  'Alpine White', 'Jet Black', 'Mineral Gray', 'Carbon Black',
+  'Sapphire Blue', 'Mediterranean Blue', 'Racing Red', 'Ferrari Red',
+  'British Racing Green', 'Emerald Green', 'Sunset Orange', 'Daytona Violet',
+  'Silver Metallic', 'Champagne', 'Pearl White', 'Metallic Bronze',
+  'Moonstone', 'Nardo Gray', 'Tanzanite Blue', 'Dravit Gray'
 ]
 
-colors = ['Red', 'Blue', 'Black', 'White', 'Silver', 'Gray', 'Green']
+CURRENT_YEAR = Time.current.year
 
-puts "Creating #{TARGET_COUNT} cars in batches of #{BATCH_SIZE}..."
+puts "Creating #{TARGET_COUNT} cars with variety..."
 
-(0...TARGET_COUNT).each_slice(BATCH_SIZE) do |batch|
-  start_id = batch.first + 1
-  end_id = batch.last + 1
+cars_data = TARGET_COUNT.times.map do |i|
+  make = CARS_DATA.keys.sample
+  model = CARS_DATA[make].sample
+  year = rand(2015..CURRENT_YEAR)
+  color = COLORS.sample
 
-  cars_data = batch.map do |i|
-    make, model, type = car_data.sample
-    year = rand(2015..2025)
-    color = colors.sample
-
-    # Use car-specific images from CARAPI
-    image_url = case type
-    when 'sedan'
-      "https://cdn.imagin.studio/getimage?customer=img&make=#{make.downcase}&modelFamily=#{model.downcase}&angle=23"
-    when 'suv'
-      "https://cdn.imagin.studio/getimage?customer=img&make=#{make.downcase}&modelFamily=#{model.downcase}&angle=23"
-    when 'sports'
-      "https://cdn.imagin.studio/getimage?customer=img&make=#{make.downcase}&modelFamily=#{model.downcase}&angle=23"
-    when 'pickup'
-      "https://cdn.imagin.studio/getimage?customer=img&make=#{make.downcase}&modelFamily=#{model.downcase}&angle=23"
-    when 'luxury'
-      "https://cdn.imagin.studio/getimage?customer=img&make=#{make.downcase}&modelFamily=#{model.downcase}&angle=23"
-    when 'electric'
-      "https://cdn.imagin.studio/getimage?customer=img&make=#{make.downcase}&modelFamily=#{model.downcase}&angle=23"
-    when 'hatchback'
-      "https://cdn.imagin.studio/getimage?customer=img&make=#{make.downcase}&modelFamily=#{model.downcase}&angle=23"
-    when 'wagon'
-      "https://cdn.imagin.studio/getimage?customer=img&make=#{make.downcase}&modelFamily=#{model.downcase}&angle=23"
-    end
-
-    # Add year and random color to the image URL for variety
-    image_url += "&year=#{year}&paintId=#{color.downcase}"
-
-    {
-      make: make,
-      model: model,
-      year: year,
-      price: rand(15_000..60_000) + rand.round(2),
-      description: "#{color} #{year} #{make} #{model}. Great condition, low mileage. #{rand(10_000..80_000)} miles. ID: #{i+1}",
-      image_url: image_url,
-      created_at: Time.current,
-      updated_at: Time.current
-    }
+  # Price logic based on make, year, and model
+  base_price = case make
+    when 'Porsche', 'Ferrari', 'Lamborghini'
+      rand(80_000..200_000)
+    when 'BMW', 'Mercedes', 'Audi', 'Tesla'
+      rand(45_000..120_000)
+    when 'Lexus', 'Volvo', 'Land Rover'
+      rand(40_000..90_000)
+    else
+      rand(25_000..60_000)
   end
 
-  Car.insert_all!(cars_data)
-  puts "Created batch of #{cars_data.size} cars (#{start_id} to #{end_id})"
+  # Adjust price based on year
+  year_factor = (year - 2015) / 10.0
+  price = (base_price * (1 + year_factor)).round(-3)
+
+  {
+    make: make,
+    model: model,
+    year: year,
+    price: price,
+    description: "#{color} #{year} #{make} #{model}. Excellent condition with premium features.",
+    image_url: "https://cdn.imagin.studio/getimage?customer=img&make=#{make.downcase}&modelFamily=#{model.downcase.gsub(' ', '-')}&angle=23&year=#{year}&paintId=#{color.downcase.gsub(' ', '-')}",
+    created_at: Time.current,
+    updated_at: Time.current
+  }
 end
+
+# Insert all cars in batches
+Car.insert_all!(cars_data)
 
 total_count = Car.count
 puts "\nSeeding completed!"
 puts "Total cars in database: #{total_count}"
-puts "Random sample of 5 cars:"
+puts "\nSample of cars created:"
 Car.order("RANDOM()").limit(5).each do |car|
-  puts "- #{car.year} #{car.make} #{car.model} ($#{car.price})"
-  puts "  Image: #{car.image_url}"
+  puts "- #{car.year} #{car.make} #{car.model} (#{car.description.split('.').first}) - $#{car.price.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\\1,')}"
 end
